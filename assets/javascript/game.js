dd0c4d99a37b17a6009ad289b63edacd109406c7
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-11-13 11:34:03 -0500

    Style change and JS additions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var attempts = 10;
+var words = {
+    famouspirates: ["Anne Bonny", "Blackbeard", "Calico Jack", "Mary Read", "Sir Francis Drake"],
+    fictionpirates: ["Captain Hook", "Davy Jones", "Jack Sparrow", "Long John Silver"],
+    piratetypes: ["buccaneer", "corsair", "privateer"],
+    pirateduties: ["boatswain", "captain", "first mate", "quarter master"],
+    pirateweapons: ["blunderbuss", "cannon", "cutlass", "flintlock gun", "musketoon"],
+    seamonsters: ["kraken", "mermaid", "sea serpent"],
+    miscellaneous: ["Caribbean", "gold", "rum", "scurvy", "storm"]
+}
